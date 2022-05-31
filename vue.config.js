c644daa5634291da1f72e0d0b037825e681a7e83
Alessio Palmero Aprosio <alessio@apnetwork.it>
2022-05-31 22:34:15 +0200

    publicPath in env

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: "/creender/",
+  publicPath: process.env.VUE_PUBLIC_PATH,
   transpileDependencies: true
 })
